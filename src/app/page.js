14ad1c82881a3c26ba1d6b0cc1219a3ceea15370
AmanDwivedi9335 <amandwdi@gmail.com>
2026-02-18 12:18:06 +0530

    hero updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import ReportCarouselSection from "@/components/Home/ReportCarouselSection";
 import HealthIdentitySection from "@/components/Home/HealthIdentitySection";
 import LovedOnesSection from "@/components/Home/LovedOnesSection";
 import DataControlSection from "@/components/Home/DataControlSection";
-import ContactCard from "@/components/Home/ContactCard";
 import Howitworks from "@/components/Home/Howitworks";
 import Realconsequences from "@/components/Home/Realconsequences";
 import Subscription from "@/components/Home/Subscription";
@@ -12,40 +11,39 @@ import Subscription from "@/components/Home/Subscription";
 export default function Home() {
   return (
     <>
-      {/* Hero Section */}
-      <div className="sticky h-screen z-[-1] top-[5px]">
-        <Hero />
+      {/* Sticky stack panels */}
+      <div className="relative isolate">
+        <div className="sticky top-[5px] h-screen z-0">
+          <Hero />
+        </div>
+
+        <div className="sticky top-[5px] h-screen z-0 bg-white py-6 md:py-12 px-4 md:px-8">
+          <ReportCarouselSection />
+        </div>
+
+        <div className="sticky top-[5px] h-screen z-0 bg-white py-6 md:py-12 px-4 md:px-8">
+          <HealthIdentitySection />
+        </div>
+
+        {/* <div className="sticky top-[5px] h-screen z-0">
+          <Realconsequences />
+        </div> */}
+
+        {/* <div className="sticky top-[5px] h-screen z-0">
+          <Howitworks />
+        </div> */}
+
+        <div className="sticky top-[5px] h-screen z-0 bg-white py-6 md:py-12 px-4 md:px-8">
+          <LovedOnesSection />
+        </div>
+
+        {/* <div className="sticky top-[5px] h-screen z-0">
+          <Subscription />
+        </div> */}
       </div>
 
-      {/* About Section */}
-      <div className="bg-white py-6 md:py-12 px-4 md:px-8 sticky h-screen z-[-1] top-[5px]">
-        <ReportCarouselSection />
-      </div>
-
-      {/* HealthIdentity Section */}
+      {/* Normal scrolling section after sticky stack ends */}
       <div className="bg-white py-6 md:py-12 px-4 md:px-8">
-        <HealthIdentitySection />
-      </div>
-
-      <div className="">
-        <Realconsequences />
-      </div>
-
-      <div className="">
-        <Howitworks />
-      </div>
-
-      {/* LovedOnes Section */}
-      <div className="bg-[#F3F0FF] py-6 md:py-12 px-4 md:px-8 sticky h-screen z-[-1] top-[5px]">
-        <LovedOnesSection />
-      </div>
-
-      <div className="">
-        <Subscription />
-      </div>
-
-      {/* Data Control Section */}
-      <div className="bg-[#F3F0FF] py-6 md:py-12 px-4 md:px-8">
         <DataControlSection />
       </div>
     </>

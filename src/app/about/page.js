b65799dedd45eb96f8d314e067ad13f5e0d1f3a9
AmanDwivedi9335 <amandwdi@gmail.com>
2026-02-24 17:24:22 +0530

    text updated2

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -13,7 +13,7 @@ export default function Page() {
 		},
 		{
 			Name: "Vaishnavi Reddy",
-			Designation: "CTO & Co-Founder",
+			Designation: "CTO",
 			Details:
 				"With 8+ years of leadership experience, she had driven impactful change in the healthcare device procurement industry, delivering operational efficiencies and scalable solutions. Her work is rooted in building strong healthcare ecosystems that combine technology, strategy, and execution.",
 			imgurl: "/images/founders/vaishnavi.jpg",
